59770d1af6f8fd790b439f88856a28e68b504b0f
victor200216 <160192904+victor200216@users.noreply.github.com>
2026-08-24 16:29:30 -0500

    ahora si si

--- js/script.js
+++ js/script.js
@@ -78,7 +78,8 @@ const invitados = [
   { slug: "ledis",         nombre: "Ledis",         tipo: "Ledis", cupos: 1 },
   { slug: "claudina",         nombre: "Claudina",         tipo: "Claudina", cupos: 1 },
   { slug: "jhoana",         nombre: "Jhoana",         tipo: "Jhoana", cupos: 1 },
-  { slug: "nicolas",         nombre: "Nicolás",         tipo: "Nicolás", cupos: 1 }
+  { slug: "nicolas",         nombre: "Nicolás",         tipo: "Nicolás", cupos: 1 },
+  { slug: "mariag",         nombre: "María Guebara.",         tipo: "María Guebara.", cupos: 1 }
   
 
 ];
